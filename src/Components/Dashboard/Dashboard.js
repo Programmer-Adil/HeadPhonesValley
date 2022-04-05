050a75626b4676395c48dddecb5216ed8219bb8e
programmer-adil <adc24h@gmail.com>
2022-04-05 23:32:14 +0600

    Barchart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, BarChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
 
 
 const Dashboard = () => {
-    const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },];
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
     return (
         <div className='container'>
-            const renderLineChart = (
-            <LineChart width={600} height={300} data={data}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="name" />
-                <YAxis />
-            </LineChart>
-            );
+            <div className="row">
+                <div className="col-md-6">
+                    <LineChart width={400} height={400} data={data}>
+                        <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                    </LineChart>
+                </div>
+                <div className="col-md-6">
+                    <BarChart width={400} height={400} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" fill="#8884d8" />
+                        <Bar dataKey="uv" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+            </div>
         </div>
     );
 };
