fedfd4f2897ec64e5280b623365ac2546a06399e
programmer-adil <adc24h@gmail.com>
2022-04-05 23:41:30 +0600

    blog done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog page</h1>
+        <div className='container'>
+            <div className="q1">
+                <h1>What is React Context API</h1>
+                <p>
+                Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease.
+                </p>
+            </div>
         </div>
     );
 };
